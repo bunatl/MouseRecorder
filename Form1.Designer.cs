1417e7070893d9e025431fe750fa9dcecac6ab24
Lukas Bunat <49941100+bunatl@users.noreply.github.com>
2020-07-09 23:35:08 +0200

    Replay loops done

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,174 +28,227 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MouseRecorder));
-			this.buttonRec = new System.Windows.Forms.Button();
-			this.buttonStop = new System.Windows.Forms.Button();
-			this.buttonPlay = new System.Windows.Forms.Button();
-			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-			this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
-			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-			this.recorderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-			this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
-			this.listView1 = new System.Windows.Forms.ListView();
-			this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-			this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-			this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-			this.MouseRec = new System.Windows.Forms.Timer(this.components);
-			this.MousePlay = new System.Windows.Forms.Timer(this.components);
-			this.CurrentPosition = new System.Windows.Forms.Timer(this.components);
-			this.menuStrip1.SuspendLayout();
-			this.statusStrip1.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// buttonRec
-			// 
-			this.buttonRec.Location = new System.Drawing.Point(12, 27);
-			this.buttonRec.Name = "buttonRec";
-			this.buttonRec.Size = new System.Drawing.Size(114, 35);
-			this.buttonRec.TabIndex = 0;
-			this.buttonRec.Text = "Record (F9)";
-			this.buttonRec.UseVisualStyleBackColor = true;
-			this.buttonRec.Click += new System.EventHandler(this.buttonRec_Click);
-			// 
-			// buttonStop
-			// 
-			this.buttonStop.Location = new System.Drawing.Point(132, 27);
-			this.buttonStop.Name = "buttonStop";
-			this.buttonStop.Size = new System.Drawing.Size(112, 35);
-			this.buttonStop.TabIndex = 1;
-			this.buttonStop.Text = "Stop (F10)";
-			this.buttonStop.UseVisualStyleBackColor = true;
-			this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
-			// 
-			// buttonPlay
-			// 
-			this.buttonPlay.Location = new System.Drawing.Point(250, 27);
-			this.buttonPlay.Name = "buttonPlay";
-			this.buttonPlay.Size = new System.Drawing.Size(106, 35);
-			this.buttonPlay.TabIndex = 2;
-			this.buttonPlay.Text = "Play (F8)";
-			this.buttonPlay.UseVisualStyleBackColor = true;
-			this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
-			// 
-			// contextMenuStrip1
-			// 
-			this.contextMenuStrip1.Name = "contextMenuStrip1";
-			this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
-			// 
-			// contextMenuStrip2
-			// 
-			this.contextMenuStrip2.Name = "contextMenuStrip2";
-			this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
-			// 
-			// menuStrip1
-			// 
-			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MouseRecorder));
+            this.buttonRec = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.recorderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.MouseRec = new System.Windows.Forms.Timer(this.components);
+            this.CurrentPosition = new System.Windows.Forms.Timer(this.components);
+            this.numOfReps = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.currentLoop = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numOfReps)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonRec
+            // 
+            this.buttonRec.Location = new System.Drawing.Point(12, 27);
+            this.buttonRec.Name = "buttonRec";
+            this.buttonRec.Size = new System.Drawing.Size(174, 35);
+            this.buttonRec.TabIndex = 0;
+            this.buttonRec.Text = "Record (F9)";
+            this.buttonRec.UseVisualStyleBackColor = true;
+            this.buttonRec.Click += new System.EventHandler(this.buttonRec_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(192, 27);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(164, 35);
+            this.buttonStop.TabIndex = 1;
+            this.buttonStop.Text = "Stop (F10)";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Location = new System.Drawing.Point(472, 27);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(155, 35);
+            this.buttonPlay.TabIndex = 2;
+            this.buttonPlay.Text = "Play (F8)";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.recorderToolStripMenuItem,
             this.settingsToolStripMenuItem});
-			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-			this.menuStrip1.Name = "menuStrip1";
-			this.menuStrip1.Size = new System.Drawing.Size(799, 24);
-			this.menuStrip1.TabIndex = 5;
-			this.menuStrip1.Text = "menuStrip1";
-			// 
-			// recorderToolStripMenuItem
-			// 
-			this.recorderToolStripMenuItem.Name = "recorderToolStripMenuItem";
-			this.recorderToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
-			this.recorderToolStripMenuItem.Text = "Recorder";
-			// 
-			// settingsToolStripMenuItem
-			// 
-			this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
-			this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
-			this.settingsToolStripMenuItem.Text = "Settings";
-			// 
-			// statusStrip1
-			// 
-			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(799, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // recorderToolStripMenuItem
+            // 
+            this.recorderToolStripMenuItem.Name = "recorderToolStripMenuItem";
+            this.recorderToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.recorderToolStripMenuItem.Text = "Recorder";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1});
-			this.statusStrip1.Location = new System.Drawing.Point(0, 552);
-			this.statusStrip1.Name = "statusStrip1";
-			this.statusStrip1.Size = new System.Drawing.Size(799, 22);
-			this.statusStrip1.TabIndex = 6;
-			this.statusStrip1.Text = "statusStrip1";
-			// 
-			// toolStripStatusLabel1
-			// 
-			this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-			this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
-			this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
-			// 
-			// listView1
-			// 
-			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.statusStrip1.Location = new System.Drawing.Point(0, 552);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(799, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1,
             this.columnHeader2,
             this.columnHeader3});
-			this.listView1.HideSelection = false;
-			this.listView1.Location = new System.Drawing.Point(12, 68);
-			this.listView1.Name = "listView1";
-			this.listView1.Size = new System.Drawing.Size(344, 481);
-			this.listView1.TabIndex = 7;
-			this.listView1.UseCompatibleStateImageBehavior = false;
-			this.listView1.View = System.Windows.Forms.View.Details;
-			// 
-			// columnHeader1
-			// 
-			this.columnHeader1.Text = "X";
-			// 
-			// columnHeader2
-			// 
-			this.columnHeader2.Text = "Y";
-			this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-			// 
-			// columnHeader3
-			// 
-			this.columnHeader3.Text = "Action";
-			this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-			this.columnHeader3.Width = 94;
-			// 
-			// MouseRec
-			// 
-			this.MouseRec.Interval = 10;
-			this.MouseRec.Tick += new System.EventHandler(this.MouseRec_Tick);
-			// 
-			// MousePlay
-			// 
-			this.MousePlay.Interval = 10;
-			this.MousePlay.Tick += new System.EventHandler(this.MousePlay_Tick);
-			// 
-			// CurrentPosition
-			// 
-			this.CurrentPosition.Interval = 10;
-			this.CurrentPosition.Tick += new System.EventHandler(this.CurrentPosition_Tick);
-			// 
-			// MouseRecorder
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(799, 574);
-			this.Controls.Add(this.listView1);
-			this.Controls.Add(this.statusStrip1);
-			this.Controls.Add(this.menuStrip1);
-			this.Controls.Add(this.buttonPlay);
-			this.Controls.Add(this.buttonStop);
-			this.Controls.Add(this.buttonRec);
-			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.MainMenuStrip = this.menuStrip1;
-			this.Name = "MouseRecorder";
-			this.Text = "Mouse Randomize Recorder";
-			this.Load += new System.EventHandler(this.MouseRecorder_Load);
-			this.menuStrip1.ResumeLayout(false);
-			this.menuStrip1.PerformLayout();
-			this.statusStrip1.ResumeLayout(false);
-			this.statusStrip1.PerformLayout();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 68);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(344, 481);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "X";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Y";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Action";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 94;
+            // 
+            // MouseRec
+            // 
+            this.MouseRec.Interval = 1;
+            this.MouseRec.Tick += new System.EventHandler(this.MouseRec_Tick);
+            // 
+            // CurrentPosition
+            // 
+            this.CurrentPosition.Interval = 50;
+            this.CurrentPosition.Tick += new System.EventHandler(this.CurrentPosition_Tick);
+            // 
+            // numOfReps
+            // 
+            this.numOfReps.Location = new System.Drawing.Point(582, 68);
+            this.numOfReps.Maximum = new decimal(new int[] {
+            8096,
+            0,
+            0,
+            0});
+            this.numOfReps.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numOfReps.Name = "numOfReps";
+            this.numOfReps.Size = new System.Drawing.Size(45, 20);
+            this.numOfReps.TabIndex = 8;
+            this.numOfReps.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(481, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Loops";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(564, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(12, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "/";
+            // 
+            // currentLoop
+            // 
+            this.currentLoop.AutoSize = true;
+            this.currentLoop.Location = new System.Drawing.Point(533, 70);
+            this.currentLoop.Name = "currentLoop";
+            this.currentLoop.Size = new System.Drawing.Size(13, 13);
+            this.currentLoop.TabIndex = 11;
+            this.currentLoop.Text = "1";
+            // 
+            // MouseRecorder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 574);
+            this.Controls.Add(this.currentLoop);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numOfReps);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.buttonPlay);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonRec);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MouseRecorder";
+            this.Text = "Mouse Randomize Recorder";
+            this.Load += new System.EventHandler(this.MouseRecorder_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numOfReps)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
@@ -213,11 +266,14 @@
 		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
 		private System.Windows.Forms.ListView listView1;
 		private System.Windows.Forms.Timer MouseRec;
-		private System.Windows.Forms.Timer MousePlay;
 		private System.Windows.Forms.ColumnHeader columnHeader1;
 		private System.Windows.Forms.ColumnHeader columnHeader2;
 		private System.Windows.Forms.ColumnHeader columnHeader3;
 		private System.Windows.Forms.Timer CurrentPosition;
-	}
+        private System.Windows.Forms.NumericUpDown numOfReps;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label currentLoop;
+    }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -14,8 +14,10 @@ namespace Recorder
 	{
 		List<MousePoint> points = new List<MousePoint>();
 		int currentListPosition = 0;
+		int timeDelay = 0;
 
-		[System.Runtime.InteropServices.DllImport("User32.dll")] // budeme používat tuto knihovnu
+		//capture inputs
+		[System.Runtime.InteropServices.DllImport("User32.dll")] 
 		private static extern short GetAsyncKeyState(int vKey);
 
 		public MouseRecorder()
@@ -28,47 +30,46 @@ namespace Recorder
 			CurrentPosition.Start();
 		}
 
-		private void MousePlay_Tick(object sender, EventArgs e)
-		{
-			//check for no input
+        /*UI buttons*/
 
-			/*Iterates through the list and replicates mouse movement*/
-			Cursor.Position = new Point(points[currentListPosition].getX(), points[currentListPosition].getY());
-			if (currentListPosition++ >= points.Count()-1 )
-				MousePlay.Stop();
-		}
-
-		private void buttonRec_Click(object sender, EventArgs e)
+        private void buttonRec_Click(object sender, EventArgs e)
 		{
 			points.Clear();
 			MouseRec.Start();
+			listView1.Items.Clear();
 		}
 
 		private void buttonStop_Click(object sender, EventArgs e)
 		{
 			MouseRec.Stop();
-			MousePlay.Stop();
 		}
 
 		private void buttonPlay_Click(object sender, EventArgs e)
 		{
-			currentListPosition = 0;
-			//how many times<? set accoring to something
-			MousePlay.Start();
+			Replay();
 		}
 
+
+		/*Recording timer*/
 		private void MouseRec_Tick(object sender, EventArgs e)
 		{
 			string tmp = MouseRecorder.GetBuffKeys();
 			if (tmp == "F10")
 				MouseRec.Stop();
+
 			MousePoint currentMousePoint = new MousePoint(Cursor.Position.X, Cursor.Position.Y);
 			points.Add(currentMousePoint);
 			if (tmp != "")
 			{
+				string[] delayValues = { timeDelay.ToString(), "Delay" };
+				listView1.Items.Add("").SubItems.AddRange(delayValues);
+
 				string[] rowValues = { currentMousePoint.getY().ToString(), tmp };
 				listView1.Items.Add(currentMousePoint.getX().ToString()).SubItems.AddRange(rowValues);
+
+				timeDelay = 0;
 			}
+			timeDelay++;
 		}
 
 		class MousePoint
@@ -99,23 +100,57 @@ namespace Recorder
 			return buffer;
 		}
 
+		private int GetRndNumber(int start, int end) {
+			Random rnd = new Random();
+			return rnd.Next(start, end);
+		}
+
+		/* Replay of the mouse movement*/
+		private void Replay() {
+			
+			//if no recordings, do not play
+			if (points.Count == 0)
+				return;
+
+			//allow user to move mouse away
+			System.Threading.Thread.Sleep(500);
+
+			//loop as many times as user selected
+			for (int i = 1; i <= numOfReps.Value; i++)
+			{
+				//set start position for replayeing to 0
+				currentListPosition = 0;
+				//Update number of the current loop
+				currentLoop.Text = i.ToString();
+
+				/*Iterate (move mouse) according to the its previous movemnt*/
+				/* Each move is followed by delay of random lenght */
+				for (int j = 0; j <= points.Count-1; j++)
+				{
+					Cursor.Position = new Point(points[currentListPosition].getX(), points[currentListPosition].getY());
+					//random wait
+					System.Threading.Thread.Sleep(GetRndNumber(7,14));
+					currentListPosition++;
+				}
+			}
+		}
+
+		/* Program "menu"*/
 		private void CurrentPosition_Tick(object sender, EventArgs e)
 		{
+			// log cursor position
 			toolStripStatusLabel1.Text = Cursor.Position.X + ", " + Cursor.Position.Y;
-			string tmp = MouseRecorder.GetBuffKeys();
+
+			// check for pressed keyes
+			string buttonPressed = MouseRecorder.GetBuffKeys();
 
 			//start recording
-			if (tmp == "F9")
+			if (buttonPressed == "F9")
 				buttonRec_Click(sender, e);
 
-			//stop recording
-			/*if (tmp == "F10")
-				MouseRec.Stop();*/
-
 			//replay
-			if (tmp == "F8")
-				buttonPlay_Click(sender, e);
-
+			if (buttonPressed == "F8")
+				Replay();
 		}
-	}
+    }
 }
